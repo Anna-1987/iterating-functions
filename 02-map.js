911050443977aaadb2ce484c80f71266b32009db
Anna-1987 <mag260910@gmail.com>
2025-01-17 13:09:29 +0200

    map

--- 02-map.js
+++ 02-map.js
@@ -3,16 +3,18 @@
 //     - Не изменяет оригинальный массив
 //     - озврщвает новый массив такой же длины
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
-const doubleNums = numbers.map(function (number) {
-     console.log(number);
+// const doubleNums = numbers.map(function (number) {
+//      console.log(number);
 
-    return number * 2;
-}
- );
-console.log('numbers', numbers);
-console.log('doubleNums', doubleNums);
+//     return number * 2;
+// }
+//  );
+// console.log('numbers', numbers);
+// console.log('doubleNums', doubleNums);
+
+// *****************************************************************
 
 const players = [
     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
@@ -25,3 +27,43 @@ const players = [
 
 
 //***--------------- Получаем массив имен всех играков-----------***
+
+const playerNames = players.map(player => { 
+    console.log(player);
+    return player.name;
+});
+console.log('playerNames', playerNames);
+
+
+
+const playerIds = players.map(player => player.id);
+console.log('playerIds', playerIds);
+
+
+// ***------------Увеличиваем кол-во пщинтовв каждого игрока на 10%-----**
+
+const upatedPlayers = players.map(player => {
+    return {
+        ...player,
+        points: player.points * 1.1,
+
+    };
+});
+
+console.table(upatedPlayers);
+
+
+// ***---------- Увеличить время игрока по id---------***
+
+const playerIdToUpdate = 'player-3';
+
+const upatedPlayers2 = players.map(player => {
+    if (player.id === playerIdToUpdate) {
+        return {
+            ...player,
+           timePlayed: player.timePlayed + 100,
+      };
+    }
+    return player;
+});
+console.table(upatedPlayers2);
