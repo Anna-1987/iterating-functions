7652e560ee463c186dc20eac41cc91e646fd54a9
Anna-1987 <mag260910@gmail.com>
2025-01-28 13:02:08 +0200

    chaining

--- chaining.js
+++ chaining.js
@@ -21,3 +21,21 @@ const sort = numbers
     .sort((a, b) => a - b);
 
 console.log('sort', sort);
+
+// ***** Cортируем тех кто онлайн по рангу
+        // - сначала фильтруем
+        // - потом сортируем
+
+const players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
+
+const onlinAndSorted = players
+    .filter(player => player.online)
+    .sort((a, b) => a.rank - b.rank);
+
+console.table(onlinAndSorted);
